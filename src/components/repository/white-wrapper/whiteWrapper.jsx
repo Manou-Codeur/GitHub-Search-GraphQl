5df62de5a753be9b7af08b957772d36d3b9f5035
khaled-codeur <manou-codeur@outlook.com>
2020-09-16 20:08:22 +0100

    added the github icon to the <Repositroy />

--- src/components/repository/white-wrapper/whiteWrapper.jsx
+++ src/components/repository/white-wrapper/whiteWrapper.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import InfoBoxes from "./info-boxes/infoBoxes";
 
 import "./whiteWrapper.scss";
+import gitHubIcon from "../../../assets/img/Octocat.png";
 
 const WhiteWrapper = () => {
   return (
     <div className="white-wrapper">
+      <img src={gitHubIcon} alt="github icon" />
       <div className="white-wrapper__repo-data">
         <InfoBoxes large={false} pos="left" />
 
