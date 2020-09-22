58c0c7b248afaa66329fa7bf559c3ee1425f9e12
khaled-codeur <manou-codeur@outlook.com>
2020-09-22 10:47:34 +0100

    upload to github

---
+++ build/precache-manifest.762903a29cd06fd7c82fc377e9df78c8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e88eac2caf6dac0d493d8279d681618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8be1fc171a51bfff428c",
+    "url": "/static/css/main.3d5f7f49.chunk.css"
+  },
+  {
+    "revision": "2264b74e3543c4a0a9cf",
+    "url": "/static/js/2.ca2cae42.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.ca2cae42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be1fc171a51bfff428c",
+    "url": "/static/js/main.7d4ead76.chunk.js"
+  },
+  {
+    "revision": "6af373bd941c439ec83d",
+    "url": "/static/js/runtime-main.d7af95be.js"
+  },
+  {
+    "revision": "ef468e275a2c13d0bc7156549261fde6",
+    "url": "/static/media/Octocat.ef468e27.png"
+  },
+  {
+    "revision": "00c4029bb163704714b47148087b9df6",
+    "url": "/static/media/bookmarks-outline.00c4029b.png"
+  },
+  {
+    "revision": "7c4ce288d3fee6737d4768c06ab9bf03",
+    "url": "/static/media/search.7c4ce288.png"
+  },
+  {
+    "revision": "9ca55a886c57aaff3e45a331370a1e4a",
+    "url": "/static/media/simple-codelines.9ca55a88.svg"
+  },
+  {
+    "revision": "aa9dfbdcb4257841b5287075f71b9920",
+    "url": "/static/media/star-outline.aa9dfbdc.png"
+  },
+  {
+    "revision": "6f6bbb16aec97391aefe120ec5a4e6a2",
+    "url": "/static/media/user.6f6bbb16.png"
+  }
+]);
